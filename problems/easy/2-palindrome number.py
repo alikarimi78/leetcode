45c2f109255ac2a47baf8de693124306d61ebd4b
a.karimi <ali0karimi1azari@gmail.com>
2025-02-17 22:18:43 +0330

    palindrome number

--- problems/easy/2-palindrome number.py
+++ problems/easy/2-palindrome number.py
@@ -2,13 +2,34 @@ class Solution1:
     def is_palindrome(self, x: int) -> bool:
         if x < 0:
             return False
+
         x = str(x)
+
         if len(x) < 2:
             return True
+
         revers_number_list = []
-        for i in len(x):
-            revers_number_list.insert(len(x) - 1 - i, x[i])
-        print(revers_number_list)
+
+        for i in range(len(x)):
+            revers_number_list.append(x[len(x) - 1 - i])
+
+        revers_number = "".join(revers_number_list)
+
+        if revers_number == x:
+            return True
+        else:
+            return False
 
 
-print(Solution1().is_palindrome(123))
+print(Solution1().is_palindrome(12321))
+
+
+class Solution2:
+    def is_palindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+
+print(Solution2().is_palindrome(12321))
